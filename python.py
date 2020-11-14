6678ef409c7f74deca8c9548ad3992f414a40299
abuczynska <73013025+abuczynska@users.noreply.github.com>
2020-11-14 20:37:40 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,16 @@
+
+g = 9.81  # m/s**2, przyspieszenie nor
+
+v = input (prędkość [km/h]: ')
+v = v * 60 * 60 / 1000   # przeliczenie
+while
+
+r = input(....)
+
+a = ...
+
+if a > f * g:
+    print ('niebezpiecznie')
+else:
+    print(wystarczająca siła tarcia)
+
